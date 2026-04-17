e5a6ba384b8e13c2c6d880d0ba6ca00562959f55
hamza <=>
2026-04-17 12:26:18 +0500

    db test successful

--- my-turborepo/packages/Product-db/test-db.ts
+++ my-turborepo/packages/Product-db/test-db.ts
@@ -2,8 +2,28 @@
 import {prisma} from "@repo/product-db"
 
 async function main() {
-  const result = await prisma.$queryRaw`SELECT 1 as ok`;
-  console.log("DB Connected:", result);
+  // const result = await prisma.$queryRaw`SELECT 1 as ok`;
+   const category = await prisma.category.create({
+    data: {
+      name: "Test Category",
+      slug: "test-category",
+    },
+  });
+    const product = await prisma.product.create({
+    data: {
+      name: "Test Product",
+      shortDescription: "Short desc",
+      description: "Full description",
+      price: 100,
+      sizes: ["M"],
+      colors: ["Black"],
+      images: {},
+      categorySlug: "test-category"
+    }
+  });
+
+  // console.log("DB Connected:", result);
+  console.log("Created Product:", product);
 }
 main()
   .then(() => prisma.$disconnect())
