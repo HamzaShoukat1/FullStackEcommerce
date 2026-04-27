fe6bd89c78716a5bca106d9ab3cd2586a85905b6
hamza <=>
2026-04-27 13:23:42 +0500

    a

--- my-turborepo/apps/Product-Service/src/Products/products.module.ts
+++ my-turborepo/apps/Product-Service/src/Products/products.module.ts
@@ -2,14 +2,15 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { ProductsService } from "./products.service.js";
 import { ProductsController } from "./products.controller.js";
-import { IsAuthenticatedGuard, RolesGuard } from "@repo/shared";
-import { ACCESS_SECRET } from "@repo/shared/cookiesOptions";
+import { getJwtAccessExpires, IsAuthenticatedGuard, RolesGuard } from "@repo/shared";
+import { getAccessTokenSecret } from "@repo/shared";
 
 
 @Module({
     imports: [
         JwtModule.register({
-            secret:ACCESS_SECRET
+            secret:getAccessTokenSecret(),
+            signOptions: { expiresIn: getJwtAccessExpires() as any },
         }),
     ],
     providers:[ProductsService, IsAuthenticatedGuard, RolesGuard],
